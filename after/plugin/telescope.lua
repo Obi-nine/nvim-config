cdde84de1382320a1ae79d6af6a7a3908b052443
obi-kaizen <douglasredmanc@gmail.com>
2026-05-18 09:24:27 +0200

    Latest updates dont know what

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,38 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ") });
-end)
-vim.keymap.set('n', '<leader>fm', builtin.marks)
 
+-- Toggle state
+local search_no_ignore = false
+
+local function find_files()
+  if search_no_ignore then
+    builtin.find_files({
+      find_command = {
+        "rg", "--files", "--hidden", "--no-ignore",
+        "-g", "!**/.git/*",
+        "-g", "!**/node_modules/*",
+        "-g", "!**/vendor/*",
+      },
+    })
+  else
+    builtin.find_files()
+  end
+end
+
+local function grep_string()
+  local opts = { search = vim.fn.input("Grep > ") }
+  if search_no_ignore then
+    opts.additional_args = { "--no-ignore", "--hidden" }
+  end
+  builtin.grep_string(opts)
+end
+
+local function toggle_ignore()
+  search_no_ignore = not search_no_ignore
+  vim.notify("Search no-ignore: " .. tostring(search_no_ignore), vim.log.levels.INFO)
+end
+
+vim.keymap.set('n', '<leader>pf', find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', grep_string)
+vim.keymap.set('n', '<leader>fm', builtin.marks)
+vim.keymap.set('n', '<leader>fi', toggle_ignore, { desc = "Toggle gitignore bypass" })
